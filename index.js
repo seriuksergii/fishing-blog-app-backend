d5ff6dcf85c7d3335f62fd44dbac988a5d334d23
Sergii Serdiuk <serdiuk-sergii@bustour.ua>
2025-02-16 21:05:15 +0200

    fix

--- index.js
+++ index.js
@@ -17,61 +17,75 @@ import { UserController, PostController } from './controllers/index.js';
 mongoose.set('strictQuery', true);
 
 mongoose
-  .connect(
-    'mongodb+srv://admin:0987654321@cluster0.a8vbd.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0'
-  )
+  .connect(process.env.MONGODB_URL)
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
-  const app = express();
+const app = express();
 
-  const storage = multer.diskStorage({
-    destination: (_, __, cb) => {
-      if (!fs.existsSync('uploads')) {
-        fs.mkdirSync('uploads');
-      }
-      cb(null, 'uploads');
-    },
-    filename: (_, file, cb) => {
-      cb(null, file.originalname);
-    },
-  });
-  
-  const upload = multer({ storage });
-  
-  app.use(express.json());
-  app.use(cors());
-  app.use('/uploads', express.static('uploads'));
-  
-  app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
-  app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
-  app.get('/auth/me', checkAuth, UserController.getMe);
-  
-  app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-    res.json({
-      url: `/uploads/${req.file.originalname}`,
-    });
-  });
-  
-  app.get('/tags', PostController.getLastTags);
-  
-  app.get('/posts', PostController.getAll);
-  app.get('/posts/tags', PostController.getLastTags);
-  app.get('/posts/:id', PostController.getOne);
-  app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
-  app.delete('/posts/:id', checkAuth, PostController.remove);
-  app.patch(
-    '/posts/:id',
-    checkAuth,
-    postCreateValidation,
-    handleValidationErrors,
-    PostController.update,
-  );
-  
-  app.listen(process.env.PORT || 3000, (err) => {
-    if (err) {
-      return console.log(err);
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
     }
-  
-    console.log('Server OK');
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.post(
+  '/auth/login',
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
+app.post(
+  '/auth/register',
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
   });
+});
+
+app.get('/tags', PostController.getLastTags);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
+app.get('/posts/:id', PostController.getOne);
+app.post(
+  '/posts',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.create
+);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch(
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update
+);
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Server OK');
+});
