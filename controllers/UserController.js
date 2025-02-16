427f4bdf1001868a3dca6f4b9f2deb198bdaaab7
Sergii Serdiuk <serdiuk-sergii@bustour.ua>
2025-02-17 00:14:52 +0200

    fix

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,13 +1,13 @@
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcryptjs from 'bcryptjs';
 
 import UserModel from '../models/User.js';
 
 export const register = async (req, res) => {
   try {
     const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
+    const salt = await bcryptjs.genSalt(10);
+    const hash = await bcryptjs.hash(password, salt);
 
     const doc = new UserModel({
       email: req.body.email,
@@ -25,7 +25,7 @@ export const register = async (req, res) => {
       'secret123',
       {
         expiresIn: '30d',
-      },
+      }
     );
 
     const { passwordHash, ...userData } = user._doc;
@@ -52,7 +52,10 @@ export const login = async (req, res) => {
       });
     }
 
-    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+    const isValidPass = await bcryptjs.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
 
     if (!isValidPass) {
       return res.status(400).json({
@@ -67,7 +70,7 @@ export const login = async (req, res) => {
       'secret123',
       {
         expiresIn: '30d',
-      },
+      }
     );
 
     const { passwordHash, ...userData } = user._doc;
